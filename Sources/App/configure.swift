9b6784f7d9651921c52383fa9fd568502859b987
yukiimai <dlxsmd@icloud.com>
2024-09-26 03:06:19 +0900

    add html & auto migration

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,8 +6,10 @@ import Vapor
 // configures your application
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
     app.migrations.add(CreatePlayer())
+    try app.autoMigrate().wait()
     try routes(app)
+
 }
