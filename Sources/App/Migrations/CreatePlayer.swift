9e576c73642f09331f37fe52a30d7bfa87fe27b0
yukiimai <dlxsmd@icloud.com>
2024-10-23 12:27:15 +0900

    プロパティの修正

--- Sources/App/Migrations/CreatePlayer.swift
+++ Sources/App/Migrations/CreatePlayer.swift
@@ -13,7 +13,7 @@ struct CreatePlayer: Migration {
             .id()
             .field("name", .string, .required)
             .field("score", .int, .required)
-            .field("createdAt", .string, .required)
+            .field("createdAt", .date, .required)
             .field("email", .string, .required)
             .create()
     }
